37ce281ee2eeb83ad735c5878426e1764a53ba3b
mdilawar-dev <77153555+mdilawar-dev@users.noreply.github.com>
2021-01-26 14:34:08 +0530

    added commits

--- server.js
+++ server.js
@@ -11,28 +11,28 @@ let app = express();
 app.use(bodyparser.json())
 app.use(logger('dev'))
 app.use(errorhandler())
-
+ //this will get the account
 app.get('/accounts',(req, res)=>{
 
     res.status(200).send(store.accounts)
 })
-
+// this will send the details
 app.post('/accounts',(req,res)=>{
     let newAccount = req.body;
     let id = store.accounts.length
     store.accounts.push(newAccount)
     res.status(201).send({id:id})
 })
-
+ //this will update the account
 app.put('/accounts/:id',(req,res)=>{
     store.accounts[req.params.id]= req.body
     res.status(200).send(store.accounts[req.params.id])
 })
-
+ //this will delete the account ..
 app.delete('/accounts/:id',(req,res) =>{
     store.accounts.splice(req.params.id, 1)
     res.status(204).send()
 })
 
 
-app.listen(3000)
+app.listen(3000) //this will listen on port 30000
